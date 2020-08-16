a47d6d21e81b05580e62021001bb087c92f575f8
vintagetrucker54 <vintage_trucker54@yahoo.com>
2020-08-16 16:06:20 -0400

    Create Degree.h

---
+++ Degree.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef DEGREE_H
+#define DEGREE_H
+
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+using std::string;
+using std::cout;
+
+
+enum Degree { NETWORK, SECURITY, SOFTWARE, UNKNOWN };
+static const std::string degreeProgramStrings[] = { "NETWORK", "SECURITY", "SOFTWARE", "UNKNOWN" };
+
+
+#endif // !DEGREE_H
