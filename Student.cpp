fa2b931e8bb29fe83951f732e173e6b19b363600
vintagetrucker54 <vintage_trucker54@yahoo.com>
2020-08-19 23:42:53 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,15 +1,15 @@
+#pragma once
 #include "student.h"
-#include "pch.h"
 #include "Degree.h"
-#include "roster.h"
 #include <string>
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <iomanip>
 using std::left;     
 using std::setw;     
 using std::cerr;     
 using std::string;
 using std::cout;
+using namespace std;
 
 //sets default
 Student::Student() {
@@ -20,8 +20,8 @@ Student::Student() {
 	this->age = 0;
 	this->courseDays = new int[courseArraySize];
 	for (int i = 0; i < courseArraySize; i++) this->courseDays[i] = 0;
+	this->degreeProgram = Degree::UNKNOWN;
 }
-
 //full construct
 Student::Student(string ID, string fName, string lName, string email, int age, int courseDays[], Degree degreeProgram) {
 	this->studentId = ID;
@@ -30,8 +30,9 @@ Student::Student(string ID, string fName, string lName, string email, int age, i
 	this->email = email;
 	this->age = age;
 	this->courseDays = new int[courseArraySize];
-	for (int i = 0; i < 3; i++) this->courseDays[i] = 0;
+	for (int i = 0; i < courseArraySize; i++) { this->courseDays[i] = courseDays[i]; }
 	this->degreeProgram = degreeProgram;
+
 };
 
 //accessors
@@ -41,11 +42,7 @@ string Student::getLName() { return lName; }
 string Student::getEmail() { return email; }
 int Student::getAge() { return age; }
 int* Student::getDays() { return courseDays; }
-
-Degree Student::getDegreeProgram()
-{
-	return Degree();
-}
+Degree Student::getDegree() { return degreeProgram; }
 
 
 //mutators
@@ -55,22 +52,24 @@ void Student::setLName(string lName) { this->lName = lName; }
 void Student::setEmail(string email) { this->email = email; }
 void Student::setAge(int age) { this->age = age; }
 void Student::setDays(int courseDays[]) {
-	this->courseDays = new int[courseArraySize];
-	for (int i = 0; i < 3; i++) this->courseDays[i] = courseDays[i];
+		for (int i = 0; i < courseArraySize; i++) { 
+		this->courseDays[i] = courseDays[i]; 
+	}
 }
+void Student::setDegree(Degree degreeProgram) { this->degreeProgram = degreeProgram; }
 
-void Student::setDegreeProgram(Degree degreeProgram){}
 
-//Displays all fields (NOT PROGRAM)
+//Displays fields
 void Student::print() {
 	cout << left << setw(5) << studentId;
 	cout << left << setw(25) << fName;
 	cout << left << setw(25) << lName;
 	cout << left << setw(45) << email;
+	cout << left << setw(5) << age;
 	cout << left << setw(5) << courseDays[0];
 	cout << left << setw(5) << courseDays[1];
 	cout << left << setw(5) << courseDays[2];
-	cout << left << setw(25) << degreeProgramStrings[getDegreeProgram()];
+	cout << left << setw(15) << degreeProgramStrings[(int)getDegree()] << endl;
 }
 
 Student::~Student()
