c53a3e4c5c4d812b5506462ac4a947b561f809cf
vintagetrucker54 <vintage_trucker54@yahoo.com>
2020-09-02 16:56:51 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -4,9 +4,9 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
-using std::left;     
-using std::setw;     
-using std::cerr;     
+using std::left;
+using std::setw;
+using std::cerr;
 using std::string;
 using std::cout;
 using namespace std;
@@ -18,10 +18,11 @@ Student::Student() {
 	this->lName = "";
 	this->email = "";
 	this->age = 0;
-	this->courseDays = new int[courseArraySize];
+	//this->courseDays = new int[courseArraySize];
 	for (int i = 0; i < courseArraySize; i++) this->courseDays[i] = 0;
 	this->degreeProgram = Degree::UNKNOWN;
 }
+
 //full construct
 Student::Student(string ID, string fName, string lName, string email, int age, int courseDays[], Degree degreeProgram) {
 	this->studentId = ID;
@@ -29,7 +30,6 @@ Student::Student(string ID, string fName, string lName, string email, int age, i
 	this->lName = lName;
 	this->email = email;
 	this->age = age;
-	this->courseDays = new int[courseArraySize];
 	for (int i = 0; i < courseArraySize; i++) { this->courseDays[i] = courseDays[i]; }
 	this->degreeProgram = degreeProgram;
 
@@ -52,8 +52,8 @@ void Student::setLName(string lName) { this->lName = lName; }
 void Student::setEmail(string email) { this->email = email; }
 void Student::setAge(int age) { this->age = age; }
 void Student::setDays(int courseDays[]) {
-		for (int i = 0; i < courseArraySize; i++) { 
-		this->courseDays[i] = courseDays[i]; 
+	for (int i = 0; i < courseArraySize; i++) {
+		this->courseDays[i] = courseDays[i];
 	}
 }
 void Student::setDegree(Degree degreeProgram) { this->degreeProgram = degreeProgram; }
@@ -72,12 +72,6 @@ void Student::print() {
 	cout << left << setw(15) << degreeProgramStrings[(int)getDegree()] << endl;
 }
 
-Student::~Student()
-{
-	if (courseDays != nullptr) {
-		delete[] courseDays;
-		courseDays = nullptr;
-	}
-}
+
 
 
